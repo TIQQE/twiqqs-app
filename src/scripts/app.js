99a0bf51793e741b37a5c2d9b375582681abaed7
David Borgenvik <borgenvik.david@gmail.com>
2019-04-23 21:54:22 +0200

    Implemented simple cognito example

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,6 +3,22 @@ import { } from '../components/tq-message-list.js';
 import { getTopics, getTwiqqs } from './twiqqsRepo.js';
 import { displayNotification, registerServiceWorker, initPush } from './serviceWorkerHelper.js';
 
+let hash = window.location.hash;
+if (hash.indexOf('access_token') !== -1) {
+  debugger;
+  let parts = location.hash.substring(1).split('&')
+  let id_token = parts[0].split('=')[1]
+  let access_token = parts[1].split('=')[1]
+  let expires_in = parts[2].split('=')[1]
+  let token_type = parts[3].split('=')[1]
+  let jwt = { id_token, access_token, expires_in, token_type }
+
+  localStorage.setItem('jwt', JSON.stringify(jwt))
+  location.hash = 'random'
+} else {
+  console.log(JSON.parse(localStorage.getItem('jwt')))
+}
+
 registerServiceWorker()
   .then(async () => {
     let sub = await initPush();
